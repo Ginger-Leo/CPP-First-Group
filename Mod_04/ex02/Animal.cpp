00b7be78cf377193e0dfc4724aec9911779f9fbd
Leo <leostorey85@hotmail.com>
2025-02-21 15:16:02 +0200

    updating

---
+++ Mod_04/ex02/Animal.cpp
@@ -0,0 +1,36 @@
+#include "Animal.hpp"
+
+Animal::Animal() : type("Basic Animal")
+{
+    std::cout << "default Animal constructor called\n";
+}
+Animal::Animal(Animal& ref) : type(ref.type) 
+{
+    std::cout << "default Animal copy constructor called\n";
+}
+Animal& Animal::operator=(const Animal& rhs)
+{
+    if (this != &rhs)
+    {
+        this-> type = rhs.type;
+    }
+    std::cout << "default Animal copy assignment operator called\n";
+
+    return *this;
+} 
+Animal::~Animal()
+{  
+    std::cout << "default Animal destructor called\n";
+}
+std::string Animal::getType() const
+{
+    return type;
+}
+void Animal::setType(std::string newType)
+{
+    type = newType;
+}
+void Animal::makeSound() const
+{
+    std::cout << "Generic animal noise\n";
+}
---
+++ Mod_04/ex02/Animal.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+#include <string.h>
+
+
+class Animal 
+{
+    protected:
+        std::string type;
+    private:
+
+    public:
+        Animal();
+        Animal(Animal&);
+        Animal& operator=(const Animal& rhs); 
+        virtual ~Animal();
+
+        std::string getType() const;
+        void setType(std::string);
+
+        virtual void makeSound() const;
+};
